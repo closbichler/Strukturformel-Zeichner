993e838949fe2135770d4e28c5b08bafba6b49da
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-03-09 10:35:08 +0100

    Einfügen der Concept0 in fxml

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("page0.fxml"));
         primaryStage.setTitle("Keko World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
