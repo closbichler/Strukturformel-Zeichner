03ab49174a112880865e23c6aa6396f4ae7e3cf1
JohannesRiegler <47178217+JohannesRiegler@users.noreply.github.com>
2020-03-09 09:55:29 +0100

    Button und Textfield + Input funktion erstellt

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    protected TextField text;
+
+    @FXML
+    protected Button but;
+
+    @FXML
+    public void input(){
+        System.out.println(text.getText());
+
+    }
+
+
+
+
 }
