480477b34c5960bef8f948e290746988910201e9
GeorgNestelberger <47178225+GeorgNestelberger@users.noreply.github.com>
2020-03-09 09:54:51 +0100

    commit die

---
+++ src/CanvasFkt.java
@@ -0,0 +1,2 @@
+public class CanvasFkt {
+}
--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 public class Controller {
+    @FXML
+    Canvas kekvas;
+
+    public void test(){
+        GraphicsContext gc = kekvas.getGraphicsContext2D();
+
+        gc.fillRect(10,10,10,10);
+
+
+    }
 }
