ff54772296ceddd38bc53e72504b38cc60a93869
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-03-23 10:04:43 +0100

    Ui design (#1)
    * Erstellen der UI Concepts
    
    * Einfügen der Concept0 in fxml
    
    * Ändern der Größe und des Menus
    
    * style.css Erstellen, menubar editieren
    
    * Window Dragable
    
    * Stylesheet + FXML
    
    * icons icons icons
    
    * error.fxml und export.fxml
    
    * Export...
    
    * Doc und Help
    
    * Export Window
    
    * error page und controller entfernen

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,26 @@
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Controller {
+    protected Stage stage;
+    protected double xOffset = 0;
+    protected double yOffset = 0;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void startWindowDrag(MouseEvent event) {
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+
+    public void dragWindow(MouseEvent event) {
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+    }
+
+    public void close() {
+        stage.close();
+    }
 }
---
+++ src/ExportController.java
@@ -0,0 +1,31 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class ExportController extends Controller {
+    private String initialFileName;
+
+    public void setInitialFileName(String name) { initialFileName = name; }
+
+    public void export() {
+        Stage window = new Stage();
+        Group group =  new Group();
+        Scene scene = new Scene(group);
+        window.setScene(scene);
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Exportieren");
+        fileChooser.setInitialFileName(initialFileName);
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("SVG files (*.svg)", "*.svg"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("GIF files (*.gif)", "*.gif"));
+
+        stage.close();
+        fileChooser.showSaveDialog(window);
+    }
+}
