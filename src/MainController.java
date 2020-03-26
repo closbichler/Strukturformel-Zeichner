0a850212da9abd8a5c1fc98fbc32b884cf0eb43d
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-03-26 08:07:10 +0100

    pull from canvas

--- src/MainController.java
+++ src/MainController.java
@@ -31,6 +31,8 @@ public class MainController extends Controller {
     @FXML
     Canvas canvas;
     @FXML
+    Slider slider;
+    @FXML
     Button btn_close;
     @FXML
     Button btn_minimize;
@@ -94,5 +96,54 @@ public class MainController extends Controller {
         popupWindow.getIcons().add(new Image(getClass().getResource("images/Strukturformel-Zeichner_Icon_2.png").toExternalForm()));
 
         return popupWindow;
+    }
+    
+    public void drawCanvas() {
+        boolean sizeunfit = true;
+        int canvaslen = (int)canvas.getWidth(), canvaswid = (int)canvas.getHeight(), fontsize = 150, row = 1, col = 1;
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Grid grid = new Grid(canvaslen, canvaswid, fontsize);
+
+        do {
+            try {
+                grid = new Grid(canvaslen, canvaswid, fontsize);
+                gc.setFont(Font.font("Arial", fontsize));
+                grid.drawGrid(gc);
+
+                String[] norm = {"", "H" ,"-", "H"};
+                String[][] arr = {{"H", "-", "-", "H"}, norm, norm, norm, {"", "H", "H", "-"}}, sc1 =  {{"H", "H", "H", ""}}, sc2 = {{"H", "-", "H", "H"}, {"H", "", "H", ""}};
+                ArrayList<ArrayList<String>> sc3 = new ArrayList<>();
+                sc3.add(new ArrayList<>());
+                sc3.add(new ArrayList<>());
+                sc3.get(0).addAll(Arrays.asList(sc2[0]));
+                sc3.get(1).addAll(Arrays.asList(sc2[1]));
+
+                CanvasFkt.drawChainHorWithSideChains(gc, grid, col, row, arr, new SideChainInput(Orientation.Right, 2, sc1), new SideChainInput(Orientation.Left, 5, sc2));
+                CanvasFkt.drawE(gc, grid, 8, 6, "O", "H", "HO", "H", "HO");
+                //CanvasFkt.drawChainVert(gc, grid, col ,row, "H,-,H,H", "H,H,H,");
+
+                sizeunfit = false;
+            } catch (ColIndexException e) {
+                if(e.getCol() < 0){
+                    col++;
+                }
+                else if(e.getCol() >= grid.getMaxCol()) {
+                    fontsize--;
+                }
+                gc.clearRect(0, 0, canvaslen, canvaswid);
+            } catch (RowIndexException e){
+                if(e.getRow() < 0){
+                    row++;
+                }
+                else if(e.getRow() >= grid.getMaxRow()) {
+                    fontsize--;
+                }
+                gc.clearRect(0, 0, canvaslen, canvaswid);
+            }
+        } while(sizeunfit);
+
+        slider.valueProperty().addListener(event -> {
+            kekvas.setRotate(slider.getValue());
+        });
     }
 }
