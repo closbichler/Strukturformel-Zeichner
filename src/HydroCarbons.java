941251fe39f0951d18d13a99776d1b90da1d213d
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-05-14 11:11:38 +0200

    Update HydroCarbons.java

--- src/HydroCarbons.java
+++ src/HydroCarbons.java
@@ -10,7 +10,10 @@ public enum HydroCarbons {
     nonadec(19), eicos(20), heneicos(21),
     docos(22), tricos(23), tetracos(24),
     pentacos(25), hexacos(26), heptacos(27),
-    octacos(28), nonacos(29), tricont(30);
+    octacos(28), nonacos(29), tricont(30),
+    tetracont(40), pentacont(50), hexacont(60),
+    heptacont(70), octacont(80), nonacont(90),
+    hect(100), dict(200), trict(300);
 
 
     //Found those Names for HydroCarbons from 1 to 30 on the website
