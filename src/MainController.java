b855a8ad737626fc779078fd48040e023ea82ed6
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-06-10 16:08:21 +0200

    optimize Imports

--- src/MainController.java
+++ src/MainController.java
@@ -19,10 +19,7 @@ import javafx.stage.StageStyle;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainController extends Controller {
     @FXML
